d22ff72faf1e3551700c47236cbe4a0cc78096f1
chrsolr <chr.solr@gmail.com>
2025-07-20 16:11:01 -0500

    Update home screen to include the basic landing center content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Link } from "./components/ui/link";
+import { Typography } from "./components/ui/typography";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center ">
       <section className="text-center">
-        <h3>noopcall.dev</h3>
+        <Typography as="h1" className="text-3xl font-semibold lowercase">
+          <span className="text-rose-400">noop</span>
+          <span className="text-amber-300">call</span>
+          <span className="text-violet-300">.dev</span>
+        </Typography>
+
         <p>christiansoler</p>
+
         <Link href="https://github.com/chrsolr" target="_blank">
           github
         </Link>
