53df64a3a484f8cd1d5ca78418e8b95ebdb1b0ae
chrsolr <chr.solr@gmail.com>
2025-07-22 07:41:16 -0500

    WIP: Advent of Code Page

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,7 +1,10 @@
-export async function getAoCReadme(): Promise<string> {
+export async function getAoCReadme(): Promise<string | undefined> {
   return fetch(
     "https://raw.githubusercontent.com/chrsolr/advent-of-code/refs/heads/main/README.md",
   )
     .then((res) => res.text())
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error(err);
+      return undefined;
+    });
 }
