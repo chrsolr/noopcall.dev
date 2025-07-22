6b4a80f5709381806ce26e6faeaee28c6240de5f
chrsolr <chr.solr@gmail.com>
2025-07-22 09:47:45 -0500

    Added AoC readme file parser

--- src/pages/aoc.tsx
+++ src/pages/aoc.tsx
@@ -1,7 +1,9 @@
 import { Typography } from "@/components/ui/typography";
 import { getAoCReadme } from "@/services/github";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import orderBy from "lodash.orderby";
+import groupBy from "lodash.groupby";
 
 type AdventOfCode = {
   year: number;
@@ -16,12 +18,55 @@ function extractAcCTables(markdown: string): AdventOfCode[] {
     return [];
   }
 
-  // const lines = markdown.split("\n");
+  const aocs: AdventOfCode[] = [];
+  const pattern = /^\|\s*\d{4}\s*\|\s*\d{2}\s*\|/;
 
-  return [];
+  const lines = markdown.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (!line) {
+      continue;
+    }
+
+    const matched = pattern.test(line);
+    if (!matched) {
+      continue;
+    }
+
+    const splitted = line
+      .split("|")
+      .map((s) => s.trim())
+      .filter((s) => s);
+
+    const aoc = {
+      year: parseInt(splitted[0]),
+      day: parseInt(splitted[1]),
+      challengeUrl: (() => {
+        const match = splitted[3]?.match(/\(([^)]+)\)/);
+        return match ? match[1] : "";
+      })(),
+      codeUrl: (() => {
+        const match = splitted[2]?.match(/\(([^)]+)\)/);
+        return match
+          ? "https://github.com/chrsolr/advent-of-code/blob/main" +
+              match[1].slice(1)
+          : "";
+      })(),
+      lang: (() => {
+        const match = splitted[2]?.match(/\[([^\]]+)\]/);
+        return match ? match[1] : "";
+      })(),
+    };
+
+    aocs.push(aoc);
+  }
+
+  return aocs;
 }
 
 function AoC() {
+  const [, setItems] = useState<Record<string, AdventOfCode[]> | undefined>();
   const { data, isLoading } = useQuery({
     queryKey: ["aoc-readme"],
     queryFn: () => getAoCReadme(),
@@ -30,7 +75,12 @@ function AoC() {
   useEffect(() => {
     const aocItems = extractAcCTables(data!);
 
-    console.log(data, aocItems);
+    const orederedItems = orderBy(aocItems, ["year", "day"]);
+    const groupedItems = groupBy(orederedItems, "lang");
+
+    setItems(groupedItems);
+
+    console.log(groupedItems);
   }, [data]);
 
   if (isLoading) {
